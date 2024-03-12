eaf5087ffb4a6ef5193d91d7067481083fc0af46
AlperenBaris <hayaletsurucu30@gmail.com>
2024-03-12 22:15:24 +0300

    Ship Methods
    Added ship getter methods definitons

---
+++ src/Ship.cpp
@@ -0,0 +1,14 @@
+//
+// Created by alper on 12.03.2024.
+//
+
+#include "../inc/Ship.h"
+
+float Ship::GetTakenDamage() { return this->takenDamage }
+float Ship::GetSpeed() { return this->speed }
+float Ship::GetFuel() { return this->fuel }
+float Ship::GetMoney() { return this->money }
+float Ship::GetHeath() { return this->heath }
+
+
+
