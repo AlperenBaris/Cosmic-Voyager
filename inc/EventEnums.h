0372cce9147d06b5fba79bd92893515d8f2a1ade
AlperenBaris <hayaletsurucu30@gmail.com>
2024-03-13 00:27:24 +0300

    Added Enum
    This enum is for return type

---
+++ inc/EventEnums.h
@@ -0,0 +1,15 @@
+//
+// Created by alper on 12.03.2024.
+//
+
+#ifndef COSMIC_VOYAGER_EVENTENUMS_H
+#define COSMIC_VOYAGER_EVENTENUMS_H
+
+enum Events
+{
+    asteroidBelt,
+    abandonedPlanet,
+    spacePirates,
+};
+
+#endif //COSMIC_VOYAGER_EVENTENUMS_H
