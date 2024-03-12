0119571c1dd085e3499791d8ed2e25f8293a1b5f
AlperenBaris <hayaletsurucu30@gmail.com>
2024-03-12 10:59:41 +0300

    Ship Features
    Added ship features

---
+++ inc/Ship.h
@@ -0,0 +1,28 @@
+//
+// Created by alper on 12.03.2024.
+//
+
+#ifndef COSMIC_VOYAGER_SHIP_H
+#define COSMIC_VOYAGER_SHIP_H
+
+class Ship {
+public:
+
+
+protected:
+    float takenDamage;
+    float speed;
+    float fuel;
+    float money;
+    float heath;
+private:
+
+};
+
+
+
+
+
+
+
+#endif //COSMIC_VOYAGER_SHIP_H
