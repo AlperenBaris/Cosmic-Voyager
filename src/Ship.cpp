8a724d4ff8295501d8a782576fdb3b08b823e6e4
BetulCapar <betulccapar@gmail.com>
2024-03-17 17:46:04 +0300

    edit comment lines to Ship

--- src/Ship.cpp
+++ src/Ship.cpp
@@ -4,12 +4,12 @@
 
 #include "Ship.h"
 
-constexpr int spentFuel = 33;
+constexpr int spentFuel = 33; //Defined variable to avoid magic number
 
-int Ship::GetFuel() const { return this->fuel; }
-int Ship::GetMoney() const { return this->money; }
-int Ship::GetHealth() const { return this->health; }
+int Ship::GetFuel() const { return this->fuel; } //Function that shows remaining fuel
+int Ship::GetMoney() const { return this->money; } //Function that shows remaining gold
+int Ship::GetHealth() const { return this->health; } //Function that shows remaining health
 
 
-void Ship::UpdateFuel() { this->fuel -= spentFuel; }
-void Ship::UpdateMoney(int moneyChange) { this->money += moneyChange; }
+void Ship::UpdateFuel() { this->fuel -= spentFuel; } //Function that updates fuel
+void Ship::UpdateMoney(int moneyChange) { this->money += moneyChange; } // Function that updates gold
