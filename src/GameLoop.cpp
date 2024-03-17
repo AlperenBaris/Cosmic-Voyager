0e14bceb381f8deb6e63b08fa43e0c1bae00f747
BetulCapar <betulccapar@gmail.com>
2024-03-17 17:34:15 +0300

    edit comment lines to GameLoop

--- src/GameLoop.cpp
+++ src/GameLoop.cpp
@@ -7,31 +7,33 @@
 #include "Printer.h"
 #include <memory>
 
-std::shared_ptr<Ship> GameLoop::SelectShip() {
-    std::cout << "Select your ship (1: Normal, 2: Fast, 3: Strong): ";
+std::shared_ptr<Ship> GameLoop::SelectShip() 
+{
+    std::cout << "Select your ship (1: Normal, 2: Fast, 3: Strong): "; 
     int choice;
 
-    std::cin >> choice;
+    std::cin >> choice; //Player chooses the ship type 
 
-
-    switch (choice) {
-        case 1:
-            Printer::PrintNormalShipASCII();
-            return std::make_shared<NormalShip>();
-        case 2:
-            Printer::PrintFastShipASCII();
-            return std::make_shared<FastShip>();
-        case 3:
-            Printer::PrintStrongShipASCII();
-            return std::make_shared<StrongShip>();
-        default:
-            std::cout << "Otomatically selected Normal Ship." << std::endl;
-            Printer::PrintNormalShipASCII();
-            return std::make_shared<NormalShip>();
+    switch (choice)
+    {
+    case 1: //If  player chooses the normal ship
+        Printer::PrintNormalShipASCII();
+        return std::make_shared<NormalShip>();
+    case 2://If the player chooses the fast ship
+        Printer::PrintFastShipASCII();
+        return std::make_shared<FastShip>();
+    case 3: //If the player chooses the strong ship
+        Printer::PrintStrongShipASCII();
+        return std::make_shared<StrongShip>();
+    default: //Default selected ship type by game is normal ship
+        std::cout << "Otomatically selected Normal Ship." << std::endl;
+        Printer::PrintNormalShipASCII();
+        return std::make_shared<NormalShip>();
     }
 }
 
-void GameLoop::GetInput() {
+void GameLoop::GetInput()
+{
     std::cout << "Press Enter to continue" << std::endl;
     std::cin.get();
 }
