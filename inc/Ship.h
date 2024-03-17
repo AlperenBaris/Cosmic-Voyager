3f8b22ffc6814d8fbe802c343afb88aa16e4ae4f
BetulCapar <betulccapar@gmail.com>
2024-03-17 18:15:56 +0300

    edit comment lines to Ship header file

--- inc/Ship.h
+++ inc/Ship.h
@@ -5,18 +5,19 @@
 #ifndef COSMIC_VOYAGER_SHIP_H
 #define COSMIC_VOYAGER_SHIP_H
 
-class Ship
+/*In this section, the methods and parameters of the Ship class are defined*/
+class Ship //Interface class
 {
 public:
-    Ship() : fuel{100}, money{0}, health{100} {}
+    Ship() : fuel{100}, money{0}, health{100} {} //Initial values ​​of features
     virtual ~Ship() = default;
-    virtual float GetSpeed() const = 0;
+    virtual float GetSpeed() const = 0; //Pure virtual
     int GetFuel() const;
     int GetMoney() const;
     int GetHealth() const;
     void UpdateFuel();
     void UpdateMoney(int moneyChange);
-    virtual void UpdateHealth(int normalDamage) = 0;
+    virtual void UpdateHealth(int normalDamage) = 0; //Pure virtual 
 
 protected:
     int fuel;
